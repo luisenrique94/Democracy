5a39586672265a2f77627b2c6018de88dfb0f421
LUIS ENRIQUE CHAVEZ PORROA <luis_chavez7@usmp.pe>
2017-07-21 15:12:03 -0500

    creacion del proyecto

---
+++ Democracia/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Democracia.Startup))]
+namespace Democracia
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
